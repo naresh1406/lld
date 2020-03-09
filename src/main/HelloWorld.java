20a5c3011e54f6910f9e1dc38ea454fde4f0c4af
ngupta <naresh.gupta@agoda.com>
2020-03-09 22:13:22 +0700

    Initial commit

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ngupta
+ * Date: 9/3/2020 AD
+ * Time: 22:02
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
